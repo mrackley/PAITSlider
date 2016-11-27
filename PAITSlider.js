f8f2e9f02b8dbe2e47368c99e1de6193bdbf697e
Mark Rackley <mrackley@gmail.com>
2016-11-26 22:29:09 -0600

    Initial drop

---
+++ PAITSlider.js
@@ -0,0 +1,101 @@
+/*
+ * PAITSlider - Turn a Promted Links list in SharePoint into a Content Slider
+ * Version 1.0 
+ * @requires jQuery v1.7 or greater 
+ * @requires unslider 
+ *
+ * Copyright (c) 2016 Mark Rackley / PAIT Group
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+/**
+ * @description Turn a Promted Links list in SharePoint into a Content Slider
+ * @type jQuery
+ * @name PAITSlider
+ * @category Plugins/PAITSlider
+ * @author Mark Rackley / http://www.paitgroup.com / mrackley@paitgroup.com
+ */
+
+$.fn.PAITSlider= function (options)
+{
+
+	document.write('<div class="PAITSlider"><ul id="PAITSlides"></ul></div>');
+	
+     var opt = $.extend({}, {
+		listName: 'PromotedLinks',
+		viewTitle: 'All Promoted Links',
+		prev: "<",
+		next: ">",
+		autoplay: true,
+		infinite: true,
+		animation: 'horizontal',
+		arrows: true,
+		dots: true,
+		keys: true,
+		delay: 5000					
+    }, options);
+
+		 var url = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('" + opt.listName + "')/Views/getbytitle('" + opt.viewTitle + "')";
+
+		var call = $.ajax({
+			url: url,
+			type: "GET",
+			dataType: "json",
+			headers: {
+				Accept: "application/json;odata=verbose"
+			}
+		});
+		call.done(function (data,textStatus, jqXHR){
+			var viewQuery = data.d.ViewQuery;
+			var viewXml = '<View><Query>' + viewQuery + '</Query></View>';
+			var url = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('" + opt.listName + "')/getitems"; 
+			var query = {  
+				'query' : {
+					'__metadata': { 'type': 'SP.CamlQuery' }, 
+					'ViewXml' : viewXml  
+				}
+			};
+			var call2 = $.ajax({
+				url: url,
+				type: "POST",
+				dataType: "json",
+				data:  JSON.stringify(query),
+				headers: {
+					Accept: "application/json;odata=verbose",
+					"Content-Type": "application/json;odata=verbose",
+		            "X-RequestDigest": jQuery("#__REQUESTDIGEST").val()
+				}
+	
+			});
+			call2.done(function (data,textStatus, jqXHR){
+				for(index in data.d.results)
+				{
+						$("#PAITSlides").append("<li onclick='window.location=\"" + data.d.results[index].LinkLocation.Url + 
+						"\"' ><div style=\"background-image: url('"+data.d.results[index].BackgroundImageLocation.Url +
+						"');\" class='PAITSlide'>"+data.d.results[index].Description+"</div></li>");
+				}		
+				$('.PAITSlider').unslider({
+					autoplay: opt.autoplay,
+					infinite: opt.infinite,
+					animation: opt.animation,
+					arrows:	opt.arrows,
+					dots: opt.dots,
+					keys: opt.keys,
+					delay: opt.delay
+				})	
+				
+				$("a.prev").html(opt.prev);
+				$("a.next").html(opt.next);
+
+			});
+		
+			call2.fail(function (jqXHR,textStatus,errorThrown){
+				alert("Error retrieving Fields: " + jqXHR.responseText);
+			});
+			
+		});
+
+		call.fail(function (jqXHR,textStatus,errorThrown){
+			alert("Error retrieving view: " + jqXHR.responseText);
+		});		
+}
---
+++ PAITSliderCEWP.js
@@ -0,0 +1,61 @@
+<script type="text/javascript" src="//code.jquery.com/jquery-1.12.3.min.js"></script>
+<script type="text/javascript" src="../SiteAssets/PAITSlider.js?rev=09" ></script>
+<script type="text/javascript" src="../SiteAssets/unslider-min.js" ></script>
+<link rel="stylesheet" href="../SiteAssets/unslider.css">
+<link rel="stylesheet" href="../SiteAssets/unslider-dots.css">
+
+
+<style type="text/css">
+ 
+.PAITSlide
+{
+	height: 200px;
+	width: 500px;
+    margin: auto;
+    border: 2px solid #000000;
+}
+
+.unslider-nav ol li {
+  border: 2px solid #000;
+}
+.unslider-nav ol li.unslider-active {
+  background: #000;
+}
+
+.unslider-arrow {
+ }
+.next, .prev {
+  font-size: 24px;
+  color: #3d3d3d;
+  top: 50%;
+  width: 25px;
+  height: 25px;
+  line-height: 20px;
+  text-align: center;
+  border-radius: 24px;
+  overflow: hidden;
+  border: 2px solid #000;
+  cursor: pointer;
+}
+
+</style>
+
+<script type="text/javascript">
+
+  $().PAITSlider({
+        listName:  'PromotedLinks', //name of Promoted Links list to use for slides
+		viewTitle: 'All Promoted Links', //name of the view to use
+        prev: "<", //HTML for the previous arrow
+        next: ">", //HTML for the next arrow
+		autoplay: true, 
+		infinite: true,
+		animation: 'horizontal',
+		arrows: true,
+		dots: true,
+		keys: true,
+		delay: 3000		
+		
+    });
+    
+</script>
+
